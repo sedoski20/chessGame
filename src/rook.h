ef1cf9aa9051b56a65324917a34fc7af259a0769
Marcelo Sedoski <sedoski_@hotmail.com>
2021-09-23 22:26:08 -0300

    Added Rook header

---
+++ src/rook.h
@@ -0,0 +1,23 @@
+#ifndef ROOK_H
+#define ROOK_H
+
+#include "piece.h"
+class Rook : public Piece
+{
+public:
+    Rook(Position initialPosition);
+    std::vector<Position> getPossibleMovements(const std::vector<Position>& selfPieces, const std::vector<Position>& opponentPieces);
+
+private:
+    void getHorizontalMovements(std::vector<Position> & movements, const std::vector<Position> & selfPieces, const std::vector<Position>& opponentPieces);
+    void getVerticalMovements(std::vector<Position> & movements, const std::vector<Position> & selfPieces, const std::vector<Position>& opponentPieces);
+    
+
+    
+
+};
+
+
+
+
+#endif // ROOK_H
