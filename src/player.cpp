2d2c5a3da9036d649d0978f17ac7ab933cfa2abe
sedoski20 <sedoski_@hotmail.com>
2021-09-24 00:14:01 -0300

    Fixed bug on the pawn creation

--- src/player.cpp
+++ src/player.cpp
@@ -21,7 +21,7 @@ Player::Player(movementDirection direction, std::string playerName)
 
 void Player::createPawns(int referenceRow) 
 {
-    for(int i = 0; i < 7; i++)
+    for(int i = 0; i < 8; i++)
     {
         Position position (referenceRow, i);
         Pawn * pawn = new Pawn(position);
