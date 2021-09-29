5ca1de97d8a5eb3701b1da2f55056ebded19f51c
Marcelo Sedoski <sedoski_@hotmail.com>
2021-09-29 00:15:02 -0300

    Added some methods to rook

--- src/rook.cpp
+++ src/rook.cpp
@@ -1,26 +1,50 @@
 #include "rook.h"
 #include <iostream>
-
-Rook::Rook(Position initialPosition)
-{
-    this->currentPosition = initialPosition;
-    //TODO: Check if some iniatialization is need
-}
-
+        
 std::vector<Position> Rook::getPossibleMovements(const std::vector<Position>& selfPieces, 
                                 const std::vector<Position>& opponentPieces) 
 {
     std::vector<Position> movements;
     this->getHorizontalMovements(movements, selfPieces, opponentPieces);
+    this->getVerticalMovements(movements, selfPieces, opponentPieces);
+
+    return movements;
 }
 
 void Rook::getHorizontalMovements(std::vector<Position>& movements, 
                                 const std::vector<Position>& selfPieces, 
                                 const std::vector<Position>& opponentPieces) 
 {
-    for(int i = currentPosition.column; i > 0; --i)
+    //Look from the current position to left, stop if some self piece or opponnent is on the position
+    for(int i = currentPosition.column - 1; i >= 0; i--)
     {
-        std::cout << "Coluna: " << i;
+        Position position(currentPosition.row, i);
+
+        //is some self piece on position?
+        if(Piece::hasPieceOnPosition(selfPieces, position))
+            break;
+
+        movements.push_back(position);
+
+        //is some opponent piece on position?
+        if(Piece::hasPieceOnPosition(opponentPieces, position))
+            break;
+    }
+
+    //Look from the current position to right, stop if some self piece or opponnent is on the position
+    for(int i = currentPosition.column + 1; i <= 7; i++)
+    {
+        Position position(currentPosition.row, i);
+
+        //is some self piece on position?
+        if(Piece::hasPieceOnPosition(selfPieces, position))
+            break;
+
+        movements.push_back(position);
+
+        //is some opponent piece on position?
+        if(Piece::hasPieceOnPosition(opponentPieces, position))
+            break;
     }
 }
 
@@ -28,6 +52,36 @@ void Rook::getVerticalMovements(std::vector<Position>& movements,
                                 const std::vector<Position>& selfPieces, 
                                 const std::vector<Position>& opponentPieces) 
 {
-    
+    //Look from the current position to bottom, stop if some self piece or opponnent is on the position
+    for(int i = currentPosition.row - 1; i >= 0; i--)
+    {
+        Position position(i, currentPosition.column);
+
+        //is some self piece on position?
+        if(Piece::hasPieceOnPosition(selfPieces, position))
+            break;
+
+        movements.push_back(position);
+
+        //is some opponent piece on position?
+        if(Piece::hasPieceOnPosition(opponentPieces, position))
+            break;
+    }
+
+    //Look from the current position to right, stop if some self piece or opponnent is on the position
+    for(int i = currentPosition.row + 1; i <= 7; i++)
+    {
+        Position position(i, currentPosition.column);
+
+        //is some self piece on position?
+        if(Piece::hasPieceOnPosition(selfPieces, position))
+            break;
+
+        movements.push_back(position);
+
+        //is some opponent piece on position?
+        if(Piece::hasPieceOnPosition(opponentPieces, position))
+            break;
+    }
 }
  
--- src/rook.h
+++ src/rook.h
@@ -2,15 +2,16 @@
 #define ROOK_H
 
 #include "piece.h"
+
 class Rook : public Piece
 {
 public:
-    Rook(Position initialPosition);
+    Rook(Position initialPosition) : Piece(initialPosition){};
     std::vector<Position> getPossibleMovements(const std::vector<Position>& selfPieces, const std::vector<Position>& opponentPieces);
 
 private:
-    void getHorizontalMovements(std::vector<Position> & movements, const std::vector<Position> & selfPieces, const std::vector<Position>& opponentPieces);
-    void getVerticalMovements(std::vector<Position> & movements, const std::vector<Position> & selfPieces, const std::vector<Position>& opponentPieces);
+    void getHorizontalMovements(std::vector<Position> & movements, const std::vector<Position>& selfPieces, const std::vector<Position>& opponentPieces);
+    void getVerticalMovements(std::vector<Position> & movements, const std::vector<Position>& selfPieces, const std::vector<Position>& opponentPieces);
     
 };
 
