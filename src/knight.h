141328d8b901e6b1b1bb5b7b76c679fe3e685b53
Marcelo Sedoski <sedoski_@hotmail.com>
2021-09-29 23:50:44 -0300

    Added class for knight

---
+++ src/knight.h
@@ -0,0 +1,17 @@
+#ifndef KNIGHT_H
+#define KNIGHT_H
+
+#include "piece.h"
+
+class Knight : public Piece
+{
+private:
+
+public:
+    Knight(Position initialPosition) : Piece(initialPosition){};
+    std::vector<Position> getPossibleMovements(const std::vector<Position>& selfPieces, const std::vector<Position>& opponentPieces);
+};
+
+
+
+#endif // KNIGHT_H
