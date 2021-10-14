aee4634a95a3419393b8bec9259977e0a92754ce
Marcelo Sedoski <sedoski_@hotmail.com>
2021-10-14 00:20:43 -0300

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "game.h"
+#include "iplayer.h"
+#include "player.h"
+#include "IBoard.h"
+#include "Board.h"
 
 int main()
 {
-    std::cout << "Hello World";
+
+    Player player1(MovementDirection::MOVING_UP);
+    Player player2(MovementDirection::MOVING_DOWN);
+    IPlayer *iplayer1 = &player1;
+    IPlayer *iplayer2 = &player2;
+
+    Game game(iplayer1, iplayer2);
+    game.selectPosition(Position(0,0));
+
     return 0;
+
 }
