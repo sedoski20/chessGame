a4fc29f387e07a45ca0a1385d0a80df18f726825
Marcelo Sedoski <sedoski_@hotmail.com>
2021-10-16 00:13:02 -0300

    ups, forgoted interface

--- src/iplayer.h
+++ src/iplayer.h
@@ -9,14 +9,14 @@ private:
 public:
     virtual int getActivePieces() = 0;
     virtual Piece *getSelectedPiece() = 0;
-    virtual std::vector<Position> getPositions() = 0;
+    virtual std::list<Position> getPositions() = 0;
     virtual bool isPieceSelected() = 0;
     virtual bool isAttack(Position position) = 0;
     virtual bool selectPiece(Position position) = 0;
     virtual bool receiveAttack(Position position) = 0;
-    virtual bool moveSelectedPiece(std::vector<Position> &opponentPieces, Position destination) = 0;
-    virtual bool isPossibleMovement(std::vector<Position> &opponentPieces, Position destination) = 0;
-    virtual bool getPossibleMovements(std::vector<Position> &opponentPieces, std::vector<Position> &possibeMovements) = 0;
+    virtual bool moveSelectedPiece(std::list<Position> &opponentPieces, Position destination) = 0;
+    virtual bool isPossibleMovement(std::list<Position> &opponentPieces, Position destination) = 0;
+    virtual bool getPossibleMovements(std::list<Position> &opponentPieces, std::list<Position> &possibeMovements) = 0;
 };
 
 
