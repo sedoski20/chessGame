412953177f7fcce8bd5e7ec9e328e4e20b2bf513
Marcelo Sedoski <sedoski_@hotmail.com>
2021-11-07 18:44:47 -0300

    Implemented some funcions in board class

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,11 +1,33 @@
 #include "board.h"
 
+Board::Board()
+{
+    boardStatus.clear();
+    player1Pieces.clear();
+    player2Pieces.clear();
+}
+
 void Board::updateBoardStatus(std::list<Position> & possibeMovements) 
 {
-    
+    this->resetBoardStatus();
+
+    for (auto &movement : possibeMovements)
+    {
+        PositionStatus status;
+        status.position = movement;
+        status.status = Status::HIGHLIGHTED;
+        
+        this->boardStatus.push_back(status); 
+    }
+
+}
+
+std::list<PositionStatus> Board::getBoardStatus ()
+{
+    return this->boardStatus;
 }
 
 void Board::resetBoardStatus() 
 {
-
+    this->boardStatus.clear();
 }
--- src/Board.h
+++ src/Board.h
@@ -4,30 +4,25 @@
 #include "iboard.h"
 #include "iplayer.h"
 
-enum class PositionStatus
-{
-    NORMAL,
-    HIGHLIGHTED,
-    ATTACK
-};
-
 class Board : public IBoard
 {
 
 private:
     std::list<Position> player1Pieces;
     std::list<Position> player2Pieces;
-    std::list<PositionStatus> highlihtedPositions;
+    std::list<PositionStatus> boardStatus;
 
 public:
-
+    Board();    
     std::list<Position> getPlayer1Positions() { return player1Pieces; }
     void setPlayer1Positions(std::list<Position> & positions) { player1Pieces = positions; }
 
     std::list<Position> getPlayer2Positions() { return player2Pieces; }
     void setPlayer2Positions(std::list<Position> & positions) { player2Pieces = positions; }
 
+    std::list<PositionStatus> getBoardStatus ();
     void updateBoardStatus(std::list<Position> & possibeMovements);
+    
     void resetBoardStatus();
 };
 
