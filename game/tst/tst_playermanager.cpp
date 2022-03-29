aa002ebfd275dc08b54ec7ee61f9421e1d31b487
Marcelo Sedoski <sedoski_@hotmail.com>
2022-03-29 14:34:16 +0200

    adding unit tests for player manager

---
+++ game/tst/tst_playermanager.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+#include "playermanager.h"
+#include "player.h"
+
+TEST(PlayerManagerTestCase, ConstructorTest)
+{
+    IPlayer *player1 = new Player(MovementDirection::MOVING_UP);
+    IPlayer *player2 = new Player(MovementDirection::MOVING_UP);
+    PlayerTurn turn = PlayerTurn::TURN_PLAYER1;
+
+    PlayerManager players(player1, player2, &turn);
+
+    EXPECT_EQ(players.getCurrentPlayer(), player1);
+    EXPECT_EQ(players.getOpponentPlayer(), player2);
+    EXPECT_EQ(players.getPlayer1(), player1);
+    EXPECT_EQ(players.getPlayer2(), player2);
+}
+
+TEST(PlayerManagerTestCase, UpdatingTurn)
+{
+    IPlayer *player1 = new Player(MovementDirection::MOVING_UP);
+    IPlayer *player2 = new Player(MovementDirection::MOVING_UP);
+    PlayerTurn turn = PlayerTurn::TURN_PLAYER1;
+
+    PlayerManager players(player1, player2, &turn);
+
+    EXPECT_EQ(players.getCurrentPlayer(), player1);
+    EXPECT_EQ(players.getOpponentPlayer(), player2);
+    EXPECT_EQ(players.getPlayer1(), player1);
+    EXPECT_EQ(players.getPlayer2(), player2);
+
+    turn = PlayerTurn::TURN_PLAYER2;
+
+    EXPECT_EQ(players.getCurrentPlayer(), player2);
+    EXPECT_EQ(players.getOpponentPlayer(), player1);
+    EXPECT_EQ(players.getPlayer1(), player1);
+    EXPECT_EQ(players.getPlayer2(), player2);
+}
