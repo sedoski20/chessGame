0a9ffff0ee9894cef2694fdbc5f69e61ec8c572b
Marcelo Sedoski <sedoski_@hotmail.com>
2022-03-29 11:43:27 +0200

    added playersmanager object to the game class

--- game/src/game.cpp
+++ game/src/game.cpp
@@ -1,18 +1,22 @@
 #include "game.h"
 #include <iostream>
 #include "board.h"
+#include "playermanager.h"
+#include "player.h"
 
 Game::Game()
 {
-    this->board = new Board();
+    IPlayer *player1 = new Player(MovementDirection::MOVING_UP);
+    IPlayer *player2 = new Player(MovementDirection::MOVING_DOWN);
+    PlayerManager *players = new PlayerManager(player1, player2);
+
+    this->board = new Board(players);
     this->status = GameStatus::PLAYING;
-    this->board->updateBoardStatus();
 }
 
 void Game::firstClick(Position position) 
 {
     board->select(position);
-    this->board->updateBoardStatus();
 }
 
 void Game::secondClick(Position position) 
@@ -20,12 +24,11 @@ void Game::secondClick(Position position)
     bool success = board->moveSelectedPiece(position);
 
     this->board->unslect();
-    this->board->updateBoardStatus();
     
     if(!success)
         return;
         
-    this->board->updateTurn();
+    // this->board->updateTurn();
 
     //After a movement the next player can be in a check mate, so
     //Check if the game is over and set the game Status
