9468baea8c0d3e3628a2859dab431662514ae656
Marcelo Sedoski <sedoski_@hotmail.com>
2022-04-01 11:12:15 +0200

    removing unecesary class

--- game/src/boardinformation.cpp
+++
@@ -1,6 +0,0 @@
-#include "boardinformation.h"
-
-BoardInformation::BoardInformation(const std::list<PieceInfo> &currentPlayerInformation,
-                                   const std::list<PieceInfo> &opponentPlayerInformation)
-                                   : currentPlayerInformation(),
-                                    opponentPlayerInformation(){}
