c345288f0d4b65d8e08c5230f1e6e7f8ccb3c22d
Marcelo Sedoski <sedoski_@hotmail.com>
2022-04-04 14:55:49 +0200

    adding static const and others definitions to ensure multiple including  on boardengine_testcases

--- game/tst/boardengine_testcases_data.h
+++ game/tst/boardengine_testcases_data.h
@@ -1,3 +1,6 @@
+#ifndef BOARDENGINE_TESTCASES_DATA_H
+#define BOARDENGINE_TESTCASES_DATA_H
+
 #include "player.h"
 #include "pawn.h"
 #include "rook.h"
@@ -8,160 +11,160 @@
 
 namespace kingEscaping
 {
-    Piece *selected_piece = new King(Position(0,0));
+    static const Piece *selected_piece = new King(Position(0,0));
 
-    std::list<const Piece*> current_player = 
+    static const std::list<const Piece*> current_player = 
     {
         selected_piece
     };
 
-    std::list<const Piece*> opponent_player = 
+    static const std::list<const Piece*> opponent_player = 
     {
         new Queen(Position(4,0))
     };
 
-    std::list<Position> possible_movements_expected = 
+    static const std::list<Position> possible_movements_expected = 
     {
         Position(0,1),
         Position(1,1)
     };
 
-    Position king_position(0,0);
+    static const Position king_position(0,0);
 }
 
 namespace kingAttacking
 {
-    Piece *selected_piece = new King(Position(0,0));
+    static const Piece *selected_piece = new King(Position(0,0));
 
-    std::list<const Piece*> current_player = 
+    static const std::list<const Piece*> current_player = 
     {
         selected_piece
     };
 
-    std::list<const Piece*> opponent_player = 
+    static const std::list<const Piece*> opponent_player = 
     {
         new Queen(Position(1,0))
     };
 
-    std::list<Position> possible_movements_expected = 
+    static const std::list<Position> possible_movements_expected = 
     {
         Position(1,0),
-        Position(0,1),
-        Position(1,1)
     };
 
-    Position king_position(0,0);
+    static const Position king_position(0,0);
 }
 
 namespace kingProtectionFence
 {
-    Piece *selected_piece = new Pawn(Position(1,1));
+    static const Piece *selected_piece = new Pawn(Position(1,1));
 
-    std::list<const Piece*> current_player = 
+    static const std::list<const Piece*> current_player = 
     {
         selected_piece,
         new King(Position(1,0))
     };
 
-    std::list<const Piece*> opponent_player = 
+    static const std::list<const Piece*> opponent_player = 
     {
         new Queen(Position(3,2))
     };
 
-    std::list<Position> possible_movements_expected = 
+    static const std::list<Position> possible_movements_expected = 
     {
         Position(2,1),
     };
 
-    Position king_position(1,0);
+    static const Position king_position(1,0);
 }
 
 namespace kingProtectionAttack
 {
-    Piece *selected_piece = new Pawn(Position(1,1));
+    static const Piece *selected_piece = new Pawn(Position(1,1));
 
-    std::list<const Piece*> current_player = 
+    static const std::list<const Piece*> current_player = 
     {
         selected_piece,
         new King(Position(2,0))
     };
 
-    std::list<const Piece*> opponent_player = 
+    static const std::list<const Piece*> opponent_player = 
     {
         new Queen(Position(2,2))
     };
 
-    std::list<Position> possible_movements_expected = 
+    static const std::list<Position> possible_movements_expected = 
     {
         Position(2,1),
         Position(2,2),
     };
 
-    Position king_position(2,0);
+    static const Position king_position(2,0);
 }
 
 namespace noSafeMovements1
 {
-    Piece *selected_piece = new Pawn(Position(1,1));
+    static const Piece *selected_piece = new Pawn(Position(1,1));
 
-    std::list<const Piece*> current_player = 
+    static const std::list<const Piece*> current_player = 
     {
         selected_piece,
         new King(Position(1,0))
     };
 
-    std::list<const Piece*> opponent_player = 
+    static const std::list<const Piece*> opponent_player = 
     {
         new Queen(Position(1,2))
     };
 
-    std::list<Position> possible_movements_expected = 
+    static const std::list<Position> possible_movements_expected = 
     {
     };
 
-    Position king_position(1,0);
+    static const Position king_position(1,0);
 }
 
 namespace noSafeMovements2
 {
-    Piece *selected_piece = new Pawn(Position(1,5));
+    static const Piece *selected_piece = new Pawn(Position(1,5));
 
-    std::list<const Piece*> current_player = 
+    static const std::list<const Piece*> current_player = 
     {
         selected_piece,
         new King(Position(1,0))
     };
 
-    std::list<const Piece*> opponent_player = 
+    static const std::list<const Piece*> opponent_player = 
     {
         new Queen(Position(1,2))
     };
 
-    std::list<Position> possible_movements_expected = 
+    static const std::list<Position> possible_movements_expected = 
     {
     };
 
-    Position king_position(1,0);
+    static const Position king_position(1,0);
 }
 
 namespace checkMate
 {
-    Piece *selected_piece = new King(Position(0,0));
+    static const Piece *selected_piece = new King(Position(0,0));
 
-    std::list<const Piece*> current_player = 
+    static const std::list<const Piece*> current_player = 
     {
         selected_piece
     };
 
-    std::list<const Piece*> opponent_player = 
+    static const std::list<const Piece*> opponent_player = 
     {
         new Queen(Position(1,3)),
         new Rook(Position(0,3))
     };
 
-    std::list<Position> possible_movements_expected = 
+    static const std::list<Position> possible_movements_expected = 
     {
     };
 
-    Position king_position(0,0);
+    static const Position king_position(0,0);
 }
+
+#endif // BOARDENGINE_TESTCASES_DATA_H
